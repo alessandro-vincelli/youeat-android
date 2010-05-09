c10a2c345ec2a311af93dab412e0b79c140264d4
a.vincelli <a.vincelli@21517268-6327-dad9-1043-4703539b3e6d>
2010-05-09 19:44:50 +0000

    added map view

--- YouEat-android/src/it/av/youeat/android/RistoranteActivity.java
+++ YouEat-android/src/it/av/youeat/android/RistoranteActivity.java
@@ -23,6 +23,8 @@ import it.av.youeat.ocm.model.Tag;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.maps.MapView;
@@ -37,7 +39,6 @@ public class RistoranteActivity extends Activity {
     private TextView mTags;
     private TextView mWWW;
     private TextView mDescription;
-    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +51,14 @@ public class RistoranteActivity extends Activity {
         mTags = (TextView) findViewById(R.id.tags);
         mWWW = (TextView) findViewById(R.id.www);
         mDescription = (TextView) findViewById(R.id.description);
+        Button button = (Button) findViewById(R.id.showsMap);
+        button.setText("Shows Map");
+        button.setClickable(true);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                launchMap();
+            }
+        });
 
         Intent intent = getIntent();
         Ristorante risto = (Ristorante) intent.getSerializableExtra(SerialID.RISTORANTE);
@@ -79,6 +88,13 @@ public class RistoranteActivity extends Activity {
         }
         return tags.toString();
     }
+    
+    private void launchMap() {
+        Intent next = new Intent();
+        next.setClass(this, MapActivity.class);
+        startActivity(next);
+    }
+
 
 //    @Override
 //    protected boolean isRouteDisplayed() {
