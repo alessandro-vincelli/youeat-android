9eaedde4f1b153c53c0daa4c3af510ebfe961d9a
a.vincelli <a.vincelli@21517268-6327-dad9-1043-4703539b3e6d>
2010-05-13 18:35:01 +0000

    Fixes Issue 1 phone call

--- YouEat-android/src/it/av/youeat/android/RistoranteActivity.java
+++ YouEat-android/src/it/av/youeat/android/RistoranteActivity.java
@@ -6,7 +6,9 @@ import it.av.youeat.ocm.model.RistoranteDescriptionI18n;
 import it.av.youeat.ocm.model.Tag;
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,6 +21,8 @@ public class RistoranteActivity extends Activity {
     private TextView mWWW;
     private TextView mDescription;
     private Ristorante risto;
+    private Button showMap;
+    private Button callRisto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +38,21 @@ public class RistoranteActivity extends Activity {
         mTags = (TextView) findViewById(R.id.tags);
         mWWW = (TextView) findViewById(R.id.www);
         mDescription = (TextView) findViewById(R.id.description);
-        Button button = (Button) findViewById(R.id.showsMap);
-        button.setText("Shows Map");
-        button.setClickable(true);
-        button.setOnClickListener(new View.OnClickListener() {
+        
+        callRisto = (Button) findViewById(R.id.callRisto);
+        callRisto.setText(risto.getPhoneNumber());
+        callRisto.setClickable(true);
+        callRisto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(createTelUrl(risto.getPhoneNumber())));
+                startActivity(intent);
+            }
+        });
+        
+        showMap = (Button) findViewById(R.id.showsMap);
+        showMap.setText("Shows Map");
+        showMap.setClickable(true);
+        showMap.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 launchMap(risto);
             }
@@ -73,6 +88,17 @@ public class RistoranteActivity extends Activity {
         next.setClass(this, MapActivity.class);
         next.putExtra(SerialID.RISTORANTE, theRisto);
         startActivity(next);
+    }
+
+    
+    private String createTelUrl(String number) {
+        if (TextUtils.isEmpty(number)) {
+            return null;
+        }
+
+        StringBuilder buf = new StringBuilder("tel:");
+        buf.append(number);
+        return buf.toString();
     }
 
 }
